e677c6aef2ded9bea17f6914db9183e6cbd948e9
amran16 <maryamg99@gmail.com>
2017-03-15 15:54:01 -0700

    changed html file name

--- wiki.js
+++ wiki.js
@@ -1,34 +1,26 @@
 
 $(document).ready(function(){
 
-  $('#search').on('click', function(){
+  $('form').on('submit', function(event){
+    
+     event.preventDefault();
+
      var searchItem = $('#searchItem').val();
 
      var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=' + searchItem + '&format=json';
       
      $.getJSON(url, function(data){
-        //console.log(data);
-
-        $('#display').html('');
 
         for(var i = 0; i < data[1].length; i++){
 
-             $('#display').append('<div><a href=' + data[3][i] +'><h2>' + data[1][i] +'</h2></a>' +'<p>' + data[2][i] +
-           '</p></div');
-
+          $('#display').append('<li><a href=' + data[3][i] +'><h2>' + data[1][i] +'</h2></a>' +'<p>' + data[2][i] +
+            '</p></li>');
         }
 
-         $('#searchItem').val('');
+        $('#searchItem').val('');
 
      });
 
-  });
-
-
-  $('#searchItem').keypress(function(event){
-    if(event.which === 13){
-      $('search').click();
-    }
   });
 
 });
